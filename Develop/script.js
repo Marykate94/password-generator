4725f0a74b2ebd010b551f47c1df2958b82d8002
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-08 16:06:18 -0400

    added variables and upper case letter selection function

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,22 @@
 // Assignment code here
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numericValues = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","@","#","$","%","&","*","?"];
 
+function generatePassword() {
+  window.alert("Please generate a password")
+}
 
-// // create series of password criteria prompts
-// var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// var numericValues = ["0","1","2","3","4","5","6","7","8","9"];
-// var specialChar = ["!","@","#","$","%","&","*","?"];
+generatePassword();
+
+function selectCriteria() {
+  var upperCase = window.prompt("Please enter upper case letters")
+  console.log(upperCase);
+}
+
+selectCriteria();
+
 //  window.alert("Welcome to password generator!")
 // // prompts the user to select password criteria
 // // prompts to enter an upper case letter
@@ -49,3 +60,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
