24b93278a880c722b178aab515313b950cae2bf8
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-09 11:08:39 -0400

    had to create separate functions to tie in with write password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,71 @@
 console.log("first");
 
-// Variable Outline
+// Variables
 var charThreshold = 0;
 var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var specialChar = ["±!@#$%^&*()_+-=§£™¡¢∞§¶•ªº–≠"];
-var numericValue = ["1234567890"];
+var numeric = ["1234567890"];
 var finalPassword = "";
 
+
+function getUserChoices(); {
+ var charThreshold = prompt("How many characters would you like your password to contain?");
+
+ if (charThreshold <= 7 || charThreshold >= 129) {
+  alert("Password length must be between 8-128 characters Try again");
+  passLength = parseInt(charThreshold);
+  return charThreshold();
+
+ }
+ lowerCaseSelect = confirm("Would you like to use lowercase letters?");
+ if (lowerCaseSelect) {
+  finalPassword += lowerCase
+}
+ upperCaseSelect = confirm("Would you like to use uppercase letters?");
+ if (upperCaseSelect) {
+  finalPassword += upperCase
+ }
+ specialCharSelect = confirm("Would you like to use special characters?");
+ if (specialCharSelect) {
+  finalPassword += specialChar
+ }
+ numericSelect = confirm("Would you like to use numbers?");
+ if (numericSelect) {
+  finalPassword += numeric
+ }
+ if (!numericSelect && !specialCharSelect && !upperCaseSelect && !lowerCaseSelect); {
+  alert('You must select "confirm" from at least one of the following prompts');
+ }
+ getUserChoices();
+};
+
+ function generatePassword(); {
+   for (var i = 0; i < passwordLength; i++) {
+    password += validCharacters.charAt(Math.floor(Math.random() * validCharacters.length));
+  }
+  var results = "";
+  var passChar = "";
+  var guarChar = "";
+ }
+
+
+var results = "";
+var passChar = "";
+var guarChar = "";
+
+//  for (var i = 0; i < passwordLength; i++) {
+//    password += validCharacters.charAt(Math.floor(Math.random() * validCharacters.length));
+//  }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// Character Length Section Begin
+// var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
-  var passwordText = document.querySelector("#password");
-  var charThreshold = prompt("How many characters would you like your password to contain?")
-  passwordText.value = password;
-  if (charThreshold <= 7 || charThreshold >= 129) {
-    alert("Password length must be between 8-128 characters Try again");
-    passLength = parseInt(charThreshold);
-    return charThreshold();
-
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
   }
-  lowerCaseSelect = confirm("Would you like to use lowercase letters?");
-  if (lowerCaseSelect) {
-    finalPassword += lowerCase
-  }
-  upperCaseSelect = confirm("Would you like to use uppercase letters?");
-  if (upperCaseSelect) {
-    finalPassword += upperCase
-  }
-  specialcharselect = confirm("Would you like to use special characters?");
-  if (specialCharSelect) {
-    finalPassword += specialChar
-  }
-  numericSelect = confirm("Would you like to use numbers?");
-  if (numericSelect) {
-    finalPassword += numeric
-  }
-  if(numericSelect = false){
-    alert('You must select "confirm" from at least one of the following prompts');
-    writePassword();
-  }
-  for (var i = 0; i < charThreshold; i++) {
-    finalPassword += passwordText.value.charAt(Math.floor(Math.random() * passwordText.value.length));
-  }
-
-}
-
-
-
-// Character length Section End
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  
+  generateBtn.addEventListener("click", writePassword);
