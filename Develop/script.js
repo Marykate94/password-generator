20fe1eb7a7bc8ed2c4c5dcc9998d883eda75d48b
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2021-05-09 11:13:21 -0400

    adjusted password variable within getUserChoices variable

--- Develop/script.js
+++ Develop/script.js
@@ -6,33 +6,33 @@ var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var specialChar = ["±!@#$%^&*()_+-=§£™¡¢∞§¶•ªº–≠"];
 var numeric = ["1234567890"];
-var finalPassword = "";
+var password = "";
 
 
 function getUserChoices(); {
- var charThreshold = prompt("How many characters would you like your password to contain?");
+ charThreshold = prompt("How many characters would you like your password to contain?");
 
  if (charThreshold <= 7 || charThreshold >= 129) {
   alert("Password length must be between 8-128 characters Try again");
-  passLength = parseInt(charThreshold);
+  charThreshold = parseInt(charThreshold);
   return charThreshold();
 
  }
  lowerCaseSelect = confirm("Would you like to use lowercase letters?");
  if (lowerCaseSelect) {
-  finalPassword += lowerCase
+  password += lowerCase
 }
  upperCaseSelect = confirm("Would you like to use uppercase letters?");
  if (upperCaseSelect) {
-  finalPassword += upperCase
+  password += upperCase
  }
  specialCharSelect = confirm("Would you like to use special characters?");
  if (specialCharSelect) {
-  finalPassword += specialChar
+  password += specialChar
  }
  numericSelect = confirm("Would you like to use numbers?");
  if (numericSelect) {
-  finalPassword += numeric
+  password += numeric
  }
  if (!numericSelect && !specialCharSelect && !upperCaseSelect && !lowerCaseSelect); {
   alert('You must select "confirm" from at least one of the following prompts');
@@ -47,16 +47,11 @@ function getUserChoices(); {
   var results = "";
   var passChar = "";
   var guarChar = "";
+
+  generatePassword();
  }
 
 
-var results = "";
-var passChar = "";
-var guarChar = "";
-
-//  for (var i = 0; i < passwordLength; i++) {
-//    password += validCharacters.charAt(Math.floor(Math.random() * validCharacters.length));
-//  }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // var generateBtn = document.querySelector("#generate");
